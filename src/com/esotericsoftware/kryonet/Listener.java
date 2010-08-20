d4fab52971552e4fb087e312e3d89e52e65aeccf
Nathan Sweet <nathan.sweet@gmail.com>
2010-08-20 03:20:01 +0000

    Improved Client connection threading.

--- src/com/esotericsoftware/kryonet/Listener.java
+++ src/com/esotericsoftware/kryonet/Listener.java
@@ -10,17 +10,29 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
- * Used to be notified about connection events. These methods will be invoked on the same thread as {@link Client#update(int)} and
- * {@link Server#update(int)}. They should not block for long periods as other network activity will not be processed until they
- * return.
+ * Used to be notified about connection events.
  */
 public class Listener {
+	/**
+	 * Called when the remote end has been connected. This will be invoked before any objects are received by
+	 * {@link #received(Connection, Object)}. This will be invoked on the same thread as {@link Client#update(int)} and
+	 * {@link Server#update(int)}. This method should not block for long periods as other network activity will not be processed
+	 * until it returns.
+	 */
 	public void connected (Connection connection) {
 	}
 
+	/**
+	 * Called when the remote end is no longer connected. There is no guarantee as to what thread will invoke this method.
+	 */
 	public void disconnected (Connection connection) {
 	}
 
+	/**
+	 * Called when an object has been received from the remote end of the connection. This will be invoked on the same thread as
+	 * {@link Client#update(int)} and {@link Server#update(int)}. This method should not block for long periods as other network
+	 * activity will not be processed until it returns.
+	 */
 	public void received (Connection connection, Object object) {
 	}
 
