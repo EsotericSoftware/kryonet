b62309658b48809851625c7145125e5d3d81aa34
Nathan Sweet <nathan.sweet@gmail.com>
2011-06-30 19:34:24 +0000

    [updated] PingPong test.

--- kryonet/test/com/esotericsoftware/kryonet/PingPongTest.java
+++ kryonet/test/com/esotericsoftware/kryonet/PingPongTest.java
@@ -8,15 +8,18 @@ import com.esotericsoftware.kryo.Kryo;
 
 public class PingPongTest extends KryoNetTestCase {
 	public void testPingPong () throws IOException {
-		final Data dataTCP = new Data(true);
-		final Data dataUDP = new Data(false);
+		final Data dataTCP = new Data();
+		populateData(dataTCP, true);
+		final Data dataUDP = new Data();
+		populateData(dataUDP, false);
 
-		final Server server = new Server();
+		final Server server = new Server(16384, 8192);
 		register(server.getKryo());
 		startEndPoint(server);
 		server.bind(tcpPort, udpPort);
 		server.addListener(new Listener() {
 			public void connected (Connection connection) {
+				connection.sendTCP(new Ball());
 				connection.sendTCP(dataTCP);
 				connection.sendUDP(dataUDP); // Note UDP ping pong stops if a UDP packet is lost.
 			}
@@ -37,7 +40,7 @@ public class PingPongTest extends KryoNetTestCase {
 
 		// ----
 
-		final Client client = new Client();
+		final Client client = new Client(16384, 8192);
 		register(client.getKryo());
 		startEndPoint(client);
 		client.addListener(new Listener() {
@@ -60,7 +63,38 @@ public class PingPongTest extends KryoNetTestCase {
 		waitForThreads(5000);
 	}
 
+	private void populateData (Data data, boolean isTCP) {
+		data.isTCP = isTCP;
+
+		StringBuffer buffer = new StringBuffer();
+		for (int i = 0; i < 3000; i++)
+			buffer.append('a');
+		data.string = buffer.toString();
+
+		data.strings = new String[] {"abcdefghijklmnopqrstuvwxyz0123456789", "", null, "!@#$", "αινσϊ"};
+		data.ints = new int[] {-1234567, 1234567, -1, 0, 1, Integer.MAX_VALUE, Integer.MIN_VALUE};
+		data.shorts = new short[] {-12345, 12345, -1, 0, 1, Short.MAX_VALUE, Short.MIN_VALUE};
+		data.floats = new float[] {0, -0, 1, -1, 123456, -123456, 0.1f, 0.2f, -0.3f, (float)Math.PI, Float.MAX_VALUE,
+			Float.MIN_VALUE};
+		data.doubles = new double[] {0, -0, 1, -1, 123456, -123456, 0.1d, 0.2d, -0.3d, Math.PI, Double.MAX_VALUE, Double.MIN_VALUE};
+		data.longs = new long[] {0, -0, 1, -1, 123456, -123456, 99999999999l, -99999999999l, Long.MAX_VALUE, Long.MIN_VALUE};
+		data.bytes = new byte[] {-123, 123, -1, 0, 1, Byte.MAX_VALUE, Byte.MIN_VALUE};
+		data.chars = new char[] {32345, 12345, 0, 1, 63, Character.MAX_VALUE, Character.MIN_VALUE};
+		data.booleans = new boolean[] {true, false};
+		data.Ints = new Integer[] {-1234567, 1234567, -1, 0, 1, Integer.MAX_VALUE, Integer.MIN_VALUE};
+		data.Shorts = new Short[] {-12345, 12345, -1, 0, 1, Short.MAX_VALUE, Short.MIN_VALUE};
+		data.Floats = new Float[] {0f, -0f, 1f, -1f, 123456f, -123456f, 0.1f, 0.2f, -0.3f, (float)Math.PI, Float.MAX_VALUE,
+			Float.MIN_VALUE};
+		data.Doubles = new Double[] {0d, -0d, 1d, -1d, 123456d, -123456d, 0.1d, 0.2d, -0.3d, Math.PI, Double.MAX_VALUE,
+			Double.MIN_VALUE};
+		data.Longs = new Long[] {0l, -0l, 1l, -1l, 123456l, -123456l, 99999999999l, -99999999999l, Long.MAX_VALUE, Long.MIN_VALUE};
+		data.Bytes = new Byte[] {-123, 123, -1, 0, 1, Byte.MAX_VALUE, Byte.MIN_VALUE};
+		data.Chars = new Character[] {32345, 12345, 0, 1, 63, Character.MAX_VALUE, Character.MIN_VALUE};
+		data.Booleans = new Boolean[] {true, false};
+	}
+
 	private void register (Kryo kryo) {
+		kryo.register(Ball.class);
 		kryo.register(String[].class);
 		kryo.register(int[].class);
 		kryo.register(short[].class);
@@ -82,33 +116,25 @@ public class PingPongTest extends KryoNetTestCase {
 	}
 
 	static public class Data {
-		public String[] strings = {"abcdefghijklmnopqrstuvwxyz0123456789", "", null, "!@#$", "αινσϊ"};
-		public int[] ints = {-1234567, 1234567, -1, 0, 1, Integer.MAX_VALUE, Integer.MIN_VALUE};
-		public short[] shorts = {-12345, 12345, -1, 0, 1, Short.MAX_VALUE, Short.MIN_VALUE};
-		public float[] floats = {0, -0, 1, -1, 123456, -123456, 0.1f, 0.2f, -0.3f, (float)Math.PI, Float.MAX_VALUE, Float.MIN_VALUE};
-		public double[] doubles = {0, -0, 1, -1, 123456, -123456, 0.1d, 0.2d, -0.3d, Math.PI, Double.MAX_VALUE, Double.MIN_VALUE};
-		public long[] longs = {0, -0, 1, -1, 123456, -123456, 99999999999l, -99999999999l, Long.MAX_VALUE, Long.MIN_VALUE};
-		public byte[] bytes = {-123, 123, -1, 0, 1, Byte.MAX_VALUE, Byte.MIN_VALUE};
-		public char[] chars = {32345, 12345, 0, 1, 63, Character.MAX_VALUE, Character.MIN_VALUE};
-		public boolean[] booleans = {true, false};
-		public Integer[] Ints = {-1234567, 1234567, -1, 0, 1, Integer.MAX_VALUE, Integer.MIN_VALUE};
-		public Short[] Shorts = {-12345, 12345, -1, 0, 1, Short.MAX_VALUE, Short.MIN_VALUE};
-		public Float[] Floats = {0f, -0f, 1f, -1f, 123456f, -123456f, 0.1f, 0.2f, -0.3f, (float)Math.PI, Float.MAX_VALUE,
-			Float.MIN_VALUE};
-		public Double[] Doubles = {0d, -0d, 1d, -1d, 123456d, -123456d, 0.1d, 0.2d, -0.3d, Math.PI, Double.MAX_VALUE,
-			Double.MIN_VALUE};
-		public Long[] Longs = {0l, -0l, 1l, -1l, 123456l, -123456l, 99999999999l, -99999999999l, Long.MAX_VALUE, Long.MIN_VALUE};
-		public Byte[] Bytes = {-123, 123, -1, 0, 1, Byte.MAX_VALUE, Byte.MIN_VALUE};
-		public Character[] Chars = {32345, 12345, 0, 1, 63, Character.MAX_VALUE, Character.MIN_VALUE};
-		public Boolean[] Booleans = {true, false};
+		public String string;
+		public String[] strings;
+		public int[] ints;
+		public short[] shorts;
+		public float[] floats;
+		public double[] doubles;
+		public long[] longs;
+		public byte[] bytes;
+		public char[] chars;
+		public boolean[] booleans;
+		public Integer[] Ints;
+		public Short[] Shorts;
+		public Float[] Floats;
+		public Double[] Doubles;
+		public Long[] Longs;
+		public Byte[] Bytes;
+		public Character[] Chars;
+		public Boolean[] Booleans;
 		public boolean isTCP;
-
-		public Data () {
-		}
-
-		public Data (boolean isTCP) {
-			this.isTCP = isTCP;
-		}
 
 		public int hashCode () {
 			final int prime = 31;
@@ -130,6 +156,7 @@ public class PingPongTest extends KryoNetTestCase {
 			result = prime * result + (isTCP ? 1231 : 1237);
 			result = prime * result + Arrays.hashCode(longs);
 			result = prime * result + Arrays.hashCode(shorts);
+			result = prime * result + ((string == null) ? 0 : string.hashCode());
 			result = prime * result + Arrays.hashCode(strings);
 			return result;
 		}
@@ -156,6 +183,9 @@ public class PingPongTest extends KryoNetTestCase {
 			if (isTCP != other.isTCP) return false;
 			if (!Arrays.equals(longs, other.longs)) return false;
 			if (!Arrays.equals(shorts, other.shorts)) return false;
+			if (string == null) {
+				if (other.string != null) return false;
+			} else if (!string.equals(other.string)) return false;
 			if (!Arrays.equals(strings, other.strings)) return false;
 			return true;
 		}
