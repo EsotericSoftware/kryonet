edae33a55cda298adcc67ffa05c98f3c6e18e87b
Chris Browne <yoda2031@gmail.com>
2015-04-28 20:18:23 +0200

    Remove @Override annotation from Listener class

--- src/com/esotericsoftware/kryonet/Listener.java
+++ src/com/esotericsoftware/kryonet/Listener.java
@@ -36,24 +36,20 @@ public class Listener implements IsListener {
 	 * {@link #received(Connection, Object)}. This will be invoked on the same thread as {@link Client#update(int)} and
 	 * {@link Server#update(int)}. This method should not block for long periods as other network activity will not be processed
 	 * until it returns. */
-	@Override
 	public void connected(Connection connection) {
 	}
 
 	/** Called when the remote end is no longer connected. There is no guarantee as to what thread will invoke this method. */
-	@Override
 	public void disconnected(Connection connection) {
 	}
 
 	/** Called when an object has been received from the remote end of the connection. This will be invoked on the same thread as
 	 * {@link Client#update(int)} and {@link Server#update(int)}. This method should not block for long periods as other network
 	 * activity will not be processed until it returns. */
-	@Override
 	public void received(Connection connection, Object object) {
 	}
 
 	/** Called when the connection is below the {@link Connection#setIdleThreshold(float) idle threshold}. */
-	@Override
 	public void idle(Connection connection) {
 	}
 
